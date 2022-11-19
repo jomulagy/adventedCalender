fdd3567b14c3d154ff33e2c232e25a74bd802a31
jomulagy <jomulagy988@gmail.com>
2022-11-19 19:10:12 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -75,7 +75,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
