30286de1e76a882dde13f98082e23d71d5d3e59a
yeojinnn <daisy2love@duksung.ac.kr>
2022-11-19 11:18:06 +0900

    재업로드

---
+++ static/js/share.js
@@ -0,0 +1,9 @@
+<script>
+$('.js-click-modal').click(function(){
+  $('.container').addClass('modal-open');
+});
+
+$('.js-close-modal').click(function(){
+  $('.container').removeClass('modal-open');
+});
+</script>
