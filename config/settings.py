bd5270f10523c15f81f3c78e6384be9c0f6bf3aa
jomulagy <jomulagy988@gmail.com>
2022-11-19 19:11:40 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -72,9 +72,7 @@ ROOT_URLCONF = 'config.urls'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATES = [
     {
